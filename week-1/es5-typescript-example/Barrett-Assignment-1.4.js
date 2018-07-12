87749e7208d2f66c1926c496ae3f7dd2d929f2b3
Mary Barrett <mkbarrett.1201@gmail.com>
2018-07-12 18:18:25 -0500

    Adding Week 1

---
+++ week-1/es5-typescript-example/Barrett-Assignment-1.4.js
@@ -0,0 +1,6 @@
+var firstName = 'Mister';
+var lastName = 'Clean';
+function myName(firstName, lastName) {
+    return 'Hello ' + firstName + ' ' + lastName;
+}
+console.log(myName(firstName, lastName));
---
+++ week-1/es5-typescript-example/Barrett-Assignment-1.4.ts
@@ -0,0 +1,7 @@
+let firstName: string = 'Mister';
+let lastName: string = 'Clean';
+
+function myName(firstName: string, lastName: string): string {
+	return 'Hello ' + firstName + ' ' + lastName;
+}
+console.log (myName(firstName, lastName))
