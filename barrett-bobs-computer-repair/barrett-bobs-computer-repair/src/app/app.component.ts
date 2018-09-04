94d763819bdc45cb11ab0f7fe3df23a465b5152a
Mary Barrett <mkbarrett.1201@gmail.com>
2018-09-04 17:47:59 -0500

    Adding Partial Week 9

---
+++ barrett-bobs-computer-repair/barrett-bobs-computer-repair/src/app/app.component.ts
@@ -0,0 +1,58 @@
+import {Component, Inject} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
+export class CheckboxConfigurable {
+  checked = false;
+  indeterminate = false;
+  labelPosition = 'after';
+  disabled = false;
+}
+export class DialogOverviewExample {
+
+  animal: string;
+  name: string;
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+
+}
+@Component({
+  selector: 'unicorn',
+  templateUrl: 'unicorn.html',
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
+
